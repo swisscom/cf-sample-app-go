2c14d30529c18e575a9a3bee0c5966727b2e22ca
konstantinos <konstantinos.karampogias@swisscom.com>
2016-05-24 16:20:39 +0200

    mariadb extented example 1

--- Makefile
+++ Makefile
@@ -7,6 +7,13 @@ binary:
 cloudpush: binary
 	cf push  
 
-localpush: binary 
-	docker run -v ${PWD}/bin:/opt/bin  --env-file ./cf.env -p 4000:4000  \
+localpush: binary  
+	docker run -v ${PWD}/bin:/opt/bin --link mariadb:mariadb  --env-file ./cf.env -p 4000:4000  \
 	   	-it cloudfoundry/cflinuxfs2 /opt/bin/gocf
+
+mariadb-start:
+	docker run -d --name mariadb --env-file ./mariadb.env  -p 3306:3306/tcp mariadb  2>/dev/null || echo "MariaDB is already running (make db-stop to start from scratch)"
+		sleep 10
+
+mariadb-stop:
+	docker rm -f  mariadb || exit 0
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
@@ -13,9 +17,26 @@ var (
 )
 
 func main() {
+	log.SetOutput(os.Stdout)
 	var port string
 	if port = os.Getenv("PORT"); len(port) == 0 {
 		port = "4000"
+	}
+
+	cStr := getConnectionStr()
+	log.Printf("Connect str %s ", cStr)
+	db, err := sql.Open("mysql", cStr)
+	if err != nil {
+		log.Fatal("Can not open DB:", err)
+	}
+	defer db.Close()
+
+	var version string
+	db.QueryRow("SELECT VERSION()").Scan(&version)
+	log.Printf("Connected to :%s\n", version)
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("DB Error Ping", err.Error())
 	}
 
 	http.HandleFunc("/", defaultHandler)
@@ -25,6 +46,18 @@ func main() {
 		log.Fatal("ListenAndServe:", err)
 	}
 
+}
+
+func getConnectionStr() string {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		databaseUrl = "mysql2://userName:userPass@127.0.0.1:3306/72C61CC6BEED?reconnect=true"
+	}
+	u, err := url.Parse(databaseUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%s@tcp(%s)%s", u.User.String(), u.Host, u.Path)
 }
 
 func defaultHandler(w http.ResponseWriter, req *http.Request) {
